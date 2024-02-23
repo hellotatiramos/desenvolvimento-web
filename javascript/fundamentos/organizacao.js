bd2fd09864db3049af051b442983f81dd5dbc304
Tatiane Ramos <contatotatiramos@gmail.com>
2024-02-23 10:54:41 -0300

    atualizando projeto

---
+++ javascript/fundamentos/organizacao.js
@@ -0,0 +1,14 @@
+/* O JavaScript é organizado em sentenças de código,
+que podem ou não usar ponto e vírgula. Também é
+organizado em blocos de código, delimitados por chaves.
+Existem vários tipos de blocos, cada um com seu objetivo.
+*/
+
+console.log("Sentença de código")
+
+{
+  {
+    console.log("Olá")
+    console.log("Mundo!")
+  }
+}
